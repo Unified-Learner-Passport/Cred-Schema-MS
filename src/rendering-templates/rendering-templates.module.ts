8b2b4ca601c7fabcd78be3c770ab4f7878d19e74
KDwevedi <kanav11dwevedi@gmail.com>
2023-02-08 09:52:02 +0530

    updated yarn.lock and fixed import error

--- src/rendering-templates/rendering-templates.module.ts
+++ src/rendering-templates/rendering-templates.module.ts
@@ -3,9 +3,13 @@ import { RenderingTemplatesService } from './rendering-templates.service';
 import { RenderingTemplatesController } from './rendering-templates.controller';
 import { PrismaService } from 'src/prisma.service';
 import { VerifyTemplateService } from './verify-template.service';
+import { SchemaModule } from 'src/schema/schema.module';
+import { SchemaService } from 'src/schema/schema.service';
 
 @Module({
-  providers: [RenderingTemplatesService, PrismaService, VerifyTemplateService],
+  imports: [SchemaModule],
+  providers: [RenderingTemplatesService, PrismaService, VerifyTemplateService, SchemaService],
   controllers: [RenderingTemplatesController],
+
 })
 export class RenderingTemplatesModule {}
